3575a09e8fdfa3ba24cb2ba0ef8f770d275a2bbf
Fırat Alçın <firatalcin04@gmail.com>
2024-10-05 21:10:02 +0300

    IService Interface'sinin Oluşturulması

---
+++ AuthServer/AuthServer.Core/Services/IGenericService.cs
@@ -0,0 +1,15 @@
+﻿using SharedLibrary.Dtos;
+using System.Linq.Expressions;
+
+namespace AuthServer.Core.Services
+{
+    public interface IGenericService<TEntity,TDto> where TEntity : class where TDto : class
+    {
+        Task<Response<TDto>> GetByIdAsync(Guid id);
+        Task<Response<IEnumerable<TDto>>> GetAllAsync();
+        Task<Response<IEnumerable<TDto>>> Where(Expression<Func<TEntity, bool>> predicate);
+        Task<Response<TDto>> AddAsync(TEntity entity);
+        Task<Response<NoDataDto>> Remove(TEntity entity);
+        Task<Response<NoDataDto>> Update(TEntity entity);
+    }
+}
