ecfe4e7b4d1d9c8672e26d3376512fb392ea5b12
Fırat Alçın <firatalcin04@gmail.com>
2024-10-05 15:59:30 +0300

    EntityLerin Oluşturulması

---
+++ AuthServer/AuthServer.Core/Models/UserRefreshToken.cs
@@ -0,0 +1,9 @@
+﻿namespace AuthServer.Core.Models
+{
+    public class UserRefreshToken
+    {
+        public int UserId { get; set; }
+        public string Code { get; set; }
+        public DateTime Expiration { get; set; }
+    }
+}
