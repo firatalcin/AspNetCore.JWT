914bb0eecba37a604e7e3d4b0e587ee06c53769a
Fırat Alçın <firatalcin04@gmail.com>
2024-10-12 20:32:56 +0300

    Option Pattern

--- AuthServer/AuthServer.API/Program.cs
+++ AuthServer/AuthServer.API/Program.cs
@@ -1,4 +1,6 @@
 
+using SharedLibrary.Configurations;
+
 namespace AuthServer.API
 {
     public class Program
@@ -8,6 +10,8 @@ namespace AuthServer.API
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+
+            builder.Services.Configure<CustomTokenOption>(builder.Configuration.GetSection("TokenOption"));
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
