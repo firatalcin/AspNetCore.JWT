f53428e0266ec755c682066767e1a61ff89af70a
Fırat Alçın <firatalcin04@gmail.com>
2024-10-12 20:45:22 +0300

    TokenService 2

--- AuthServer/AuthServer.Service/Services/TokenService.cs
+++ AuthServer/AuthServer.Service/Services/TokenService.cs
@@ -7,7 +7,9 @@ using Microsoft.Extensions.Options;
 using SharedLibrary.Configurations;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
@@ -35,6 +37,21 @@ namespace AuthServer.Service.Services
             }
 
             return Convert.ToBase64String(numberByte);
+        }
+
+        private IEnumerable<Claim> GetClaim(UserApp userApp, List<string> audiences)
+        {
+            var userList = new List<Claim>() 
+            {
+                new Claim(ClaimTypes.NameIdentifier, userApp.Id),
+                new Claim(JwtRegisteredClaimNames.Email, userApp.Email),
+                new Claim(ClaimTypes.Name, userApp.UserName),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+            userList.AddRange(audiences.Select(x => new Claim(JwtRegisteredClaimNames.Aud, x)));
+
+            return userList;
         }
 
         public TokenDto CreateToken(UserApp userApp)
