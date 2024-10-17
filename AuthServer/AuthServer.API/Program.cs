f35547f9ebddd84f6c0539e89521055c4b1cf518
Fırat Alçın <firatalcin04@gmail.com>
2024-10-17 23:50:47 +0300

    Startup 1

--- AuthServer/AuthServer.API/Program.cs
+++ AuthServer/AuthServer.API/Program.cs
@@ -1,5 +1,15 @@
 
 using AuthServer.Core.Configurations;
+using AuthServer.Core.Models;
+using AuthServer.Core.Repositories;
+using AuthServer.Core.Services;
+using AuthServer.Core.UnitOfWork;
+using AuthServer.Data.Contexts;
+using AuthServer.Data.Repositories;
+using AuthServer.Data.UOW;
+using AuthServer.Service.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using SharedLibrary.Configurations;
 
 namespace AuthServer.API
@@ -11,6 +21,29 @@ namespace AuthServer.API
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+
+            builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
+            builder.Services.AddScoped<IUserService, UserService>();
+            builder.Services.AddScoped<ITokenService, TokenService>();
+            builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            builder.Services.AddScoped(typeof(IGenericService<,>), typeof(GenericService<,>));
+
+            builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            builder.Services.AddDbContext<AppDbContext>(opt =>
+            {
+                opt.UseSqlServer("server=MAKINA\\SQLEXPRESS01; database= AutServiceDb; integrated security=true;TrustServerCertificate=True", sqlOption =>
+                {
+                    sqlOption.MigrationsAssembly("UdemyAutServer.Data");
+                });
+            });
+
+            builder.Services.AddIdentity<UserApp, IdentityRole>(opt =>
+            {
+                opt.User.RequireUniqueEmail = true;
+                opt.Password.RequireNonAlphanumeric = false;
+            }).AddEntityFrameworkStores<AppDbContext>().AddDefaultTokenProviders();
+
 
             builder.Services.Configure<CustomTokenOption>(builder.Configuration.GetSection("TokenOption"));
             builder.Services.Configure<List<Client>>(builder.Configuration.GetSection("Clients"));
