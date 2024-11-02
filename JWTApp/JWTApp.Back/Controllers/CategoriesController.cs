855723ed2a59abd2cea0692a7c88b25c1bf07f03
Fırat Alçın <firatalcin04@gmail.com>
2024-11-02 20:37:53 +0300

    Category Queries

---
+++ JWTApp/JWTApp.Back/Controllers/CategoriesController.cs
@@ -0,0 +1,32 @@
+﻿using JWTApp.Back.Core.Application.Features.CQRS.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWTApp.Back.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public CategoriesController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> List()
+        {
+            var result = await _mediator.Send(new GetAllCategoriesQueryRequest());
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var result = await _mediator.Send(new GetCategoryQueryRequest(id));
+            return Ok(result);
+        }
+    }
+}
