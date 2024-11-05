3af29a4de4566bfbf71140bbd1b65bbfdd57daf3
Fırat Alçın <firatalcin04@gmail.com>
2024-11-05 23:57:17 +0300

    Register User

---
+++ JWTApp/JWTApp.Back/Controllers/AuthController.cs
@@ -0,0 +1,26 @@
+﻿using JWTApp.Back.Core.Application.Features.CQRS.Commands;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWTApp.Back.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public AuthController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost("Register")]
+        public async Task<IActionResult> Register(RegisterUserCommandRequest request)
+        {
+            await _mediator.Send(request);
+            return Ok();
+        }
+    }
+}
