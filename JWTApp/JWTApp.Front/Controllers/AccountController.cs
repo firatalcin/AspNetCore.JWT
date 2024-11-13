f9cc31fc9ee6cbac4ad59e4085ed4c56d34700dd
Fırat Alçın <firatalcin04@gmail.com>
2024-11-13 23:34:44 +0300

    SignIn 1

---
+++ JWTApp/JWTApp.Front/Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using JWTApp.Front.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWTApp.Front.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View(new UserLoginModel());
+        }
+
+        [HttpPost]
+        public IActionResult Login(UserLoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                return View();
+            }
+            return View(model);
+        }
+    }
+}
